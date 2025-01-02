7d0480502cbb81415d46a66b89256a4a9271cbb7
MADHUSUDHANKARETI <madhusudhankareti@gmail.com>
2025-01-02 23:23:04 +0530

    Initial commit

---
+++ tests/CheckBox.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('DemoQA', async ({page}) => {
+    await page.goto("https://the-internet.herokuapp.com/checkboxes")
+    // await page.locator("//input[@type='checkbox'][1]").check()
+    // await expect(await page.locator("//input[@type='checkbox'][1]")).toBeChecked()
+    // await expect(await page.locator("//input[@type='checkbox'][1]").isChecked()).toBeTruthy()
+    await expect(await page.locator("//input[@type='checkbox'][2]")).Check()
+    await expect(await page.locator("//input[@type='checkbox'][2]").isChecked()).toBeFalsy()
+
+
+    //await expect(await page.locator("//input[@type='checkbox'][2]")).toBeChecked()
+})
